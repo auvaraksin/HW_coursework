5d88fc9e5180ec7d0eb52e55737089289a36acbc
auvararaksin <auvaraksin@mail.ru>
2022-03-03 18:10:42 +0200

    HW_coursework easy level task version 1.0

--- src/pro/sky/Main.java
+++ src/pro/sky/Main.java
@@ -3,30 +3,46 @@ package pro.sky;
 public class Main {
 
     public static void main(String[] args) {
-        Employee[] employeeList = new Employee[10];
-        employeeList[0] = new Employee("Андреев Андрей Андреевич", "Отдел 001", generateRandomSalary());
-        employeeList[1] = new Employee("Борисов Борис Борисович", "Отдел 002", generateRandomSalary());
-        employeeList[2] = new Employee("Васильев Василий Васильевич", "Отдел 003", generateRandomSalary());
-        employeeList[3] = new Employee("Гвидонов Гвидон Гвидонович", "Отдел 001", generateRandomSalary());
-        employeeList[4] = new Employee("Дмитриев Дмитрий Дмитриевич", "Отдел 002", generateRandomSalary());
-        employeeList[5] = new Employee("Егоров Егор Егорович", "Отдел 003", generateRandomSalary());
-        employeeList[6] = new Employee("Жан Жан Жанович", "Отдел 001", generateRandomSalary());
-        employeeList[7] = new Employee("Захаров Захар Захарович", "Отдел 002", generateRandomSalary());
-        employeeList[8] = new Employee("Исмаилов Исмаил Исмаилович", "Отдел 003", generateRandomSalary());
-        employeeList[9] = new Employee("Кириллов Кирилл Кириллович", "Отдел 001", generateRandomSalary());
-        //code
+        Employee[] employeeList = generateEmployeeList();
         printList(employeeList);
         System.out.println("Сумма затрат на зарплату в месяц: " + calculateTotalSumMonthSalary(employeeList) + " руб.");
-        System.out.println("Сотрудник с минимальной зарплатой: " + employeeList[findMinSalary(employeeList)]);
-        System.out.println("Сотрудник с максимальной зарплатой: " + employeeList[findMaxSalary(employeeList)]);
+        System.out.println("Сотрудник с минимальной зарплатой: " + findMinSalary(employeeList));
+        System.out.println("Сотрудник с максимальной зарплатой: " + findMaxSalary(employeeList));
         System.out.println("Среднее значение зарплаты: " + calculateAverageMonthSalary(employeeList) + " руб./мес.");
         printListOfEmployee(employeeList);
     }
 
+    public static String generateRandomEmployeeName() {
+        String[] surnameList = {"Яшин", "Маслаченко", "Кесарев", "Чохели", "Масленкин", "Крутиков",
+                "Войнов", "Нетто", "Царев", "Матревели", "Иванов", "Понедельник", "Бубукин", "Ковалев", "Месхи", "Апухтин", "Калоев"};
+        String[] nameList = {"Лев", "Владимир", "Гиви", "Анатолий", "Юрий", "Игорь", "Виктор", "Слава", "Валентин", "Юрий",
+                "Михаил", "Герман","Заур"};
+        String[] patronymicList = {"Иванович", "Никитович", "Петрович", "Дмитриевич", "Евстигнеевич", "Федорович", "Николаевич", "Александрович", "Григорьевич", "Калистратович", "Козьмич", "Владимирович", "Борисович", "Шалвович"};
+        int randSurname = (int) (Math.random() * surnameList.length);
+        int randName = (int) (Math.random() * nameList.length);
+        int randPatronymic = (int) (Math.random() * patronymicList.length);
+        String employeeName = surnameList[randSurname] + " " + nameList[randName] + " " + patronymicList[randPatronymic];
+        return employeeName;
+    }
+
+    public static String generateRandomDepartment() {
+        int randomId = (int) (Math.random() * 3) + 1;
+        String departmentId = "Отдел " + randomId;
+        return departmentId;
+    }
+
     public static int generateRandomSalary() {
         java.util.Random random = new java.util.Random();
-        int randomSalary = random.nextInt(100_000);
+        int randomSalary = random.nextInt(100_000) + 100_000;
         return randomSalary;
+    }
+
+    public static Employee[] generateEmployeeList() {
+        Employee[] employeeList = new Employee[10];
+        for (int i = 0; i < employeeList.length; i++) {
+            employeeList[i] = new Employee(generateRandomEmployeeName(), generateRandomDepartment(), generateRandomSalary());
+        }
+        return employeeList;
     }
 
     public static void printList(Employee[] employeeList) {
@@ -44,7 +60,7 @@ public class Main {
         return totalSumMonthSalary;
     }
 
-    public static int findMinSalary(Employee[] employeesList) {
+    public static Employee findMinSalary(Employee[] employeesList) {
         int id = 0;
         int minSalary = employeesList[id].getEmployeeSalary();
         for (int i = 1; i < employeesList.length; i++) {
@@ -53,10 +69,10 @@ public class Main {
                 minSalary = employeesList[i].getEmployeeSalary();
             }
         }
-        return id;
+        return employeesList[id];
     }
 
-    public static int findMaxSalary(Employee[] employeesList) {
+    public static Employee findMaxSalary(Employee[] employeesList) {
         int id = 0;
         int maxSalary = employeesList[id].getEmployeeSalary();
         for (int i = 1; i < employeesList.length; i++) {
@@ -65,7 +81,7 @@ public class Main {
                 maxSalary = employeesList[i].getEmployeeSalary();
             }
         }
-        return id;
+        return employeesList[id];
     }
 
     public static float calculateAverageMonthSalary(Employee[] employeesList) {
